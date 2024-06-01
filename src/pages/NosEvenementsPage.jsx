4ad3fb1bb77c995f5461bf9927a5160170adf338
Filip Wasilewski <filipwasilewski1@gmail.com>
2024-06-01 12:25:46 +0100

    added accents

--- src/pages/NosEvenementsPage.jsx
+++ src/pages/NosEvenementsPage.jsx
@@ -2,7 +2,7 @@ export default function NosEvenementsPage() {
   return (
     <section className="min-h-[calc(100dvh-60px-40px)] font-montserrat xl:min-h-[calc(100dvh-80px-40px)]">
       <h1 className="bg-[#AF45FF] py-6 text-center text-4xl text-white antialiased lg:text-5xl">
-        Nos Evenements
+        Nos évènements
       </h1>
     </section>
   );
--- src/pages/QuiSommeNousPage.jsx
+++ src/pages/QuiSommeNousPage.jsx
@@ -1,7 +1,7 @@
 export default function QuiSommeNousPage() {
   return (
     <section className="xl:min-h-[calc(100dvh-80px-40px)]">
-      <h1 className="bg-[#AF45FF] py-6 text-center text-4xl text-white lg:text-5xl">
+      <h1 className="bg-[#AF45FF] py-6 text-center text-4xl text-white antialiased lg:text-5xl">
         Qui sommes nous ?
       </h1>
     </section>
